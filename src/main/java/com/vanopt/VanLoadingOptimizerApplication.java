a25489a27181c5ff210a6d643e7cfc4cf7fc2109
sandroonizharadze <sandroonizharadze@gmail.com>
2026-03-13 16:54:53 +0400

    Initial commit

---
+++ src/main/java/com/vanopt/VanLoadingOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.vanopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanLoadingOptimizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanLoadingOptimizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vanopt/VanLoadingOptimizerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.vanopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class VanLoadingOptimizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
